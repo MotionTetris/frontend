a52109045da4869cfa8eb96eeef933d31746df92
Sleeeeeepy <Sleeeeeepy@github.io>
2024-02-10 23:07:04 +0900

    fix: preload room socket

--- src/pages/Lobby/GameLobby.tsx
+++ src/pages/Lobby/GameLobby.tsx
@@ -1,7 +1,9 @@
 import BannerComponent from "@components/Banner/Banner";
 import { LobbyContainer } from "./styles";
+import { useRoomSocket } from "@src/context/roomSocket";
 
 const GameLobby = () => {
+  useRoomSocket();
   return (
     <LobbyContainer>
       <BannerComponent />
