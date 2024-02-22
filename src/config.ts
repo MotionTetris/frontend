12cbc3bb69a4f9966c3a8accbac9a4109e101190
Sleeeeeepy <adjustedmin@gmail.com>
2024-02-22 13:11:41 +0000

    conf: game server

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const ROOM_SOCKET_URL = "https://captainwook.site";
-export const GAME_SOCKET_URL = "ws://15.164.166.146:3001";
+export const GAME_SOCKET_URL = "https://hyungminkim.shop";
 export const ROOM_BG1_URL = "https://i.postimg.cc/y6f2C0sm/Tetris-back1.png"
 export const ROOM_BG2_URL = "https://i.postimg.cc/CLc8NLCL/Tetris-back2.png"
 export const ROOM_BG3_URL = "https://i.postimg.cc/ZRzvR9mj/Tetris-back3.png"
